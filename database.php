3169c2907cbdce293e4aeed4867bee49a579ae6b
MAKisON107 <makison1070@gmail.com>
2023-04-18 18:46:43 +0200

    oke

---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+include_once('config.php');
+
+function getConnection() {
+    global $dbHost, $dbName, $dbUser, $dbPassword;
+    $connection = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+    if($connection->connect_errno) {
+        $connection->close();
+        die('Database connection problem');
+    }
+    return $connection;
+}
+
+function getAllPosts() {
+    $connection = getConnection();
+    $sql = 'SELECT posts.id AS id posts.title AS title, categories.name AS categoryName FROM posts INNER JOIN categories ON posts.categoryId=categories.id';
+    $result = $connection->querty($sql);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+    return $rows;
+}
